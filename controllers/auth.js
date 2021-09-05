114d0ce3fd78427a1ff4c4e85238589182c8fc24
swaperX <mesut.nedzhib@gmail.com>
2021-09-05 17:35:42 +0300

    commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,19 @@
-const register = (req, res, next) => {
+const User = require("../models/User");
+
+const register = async (req, res, next) => {
+  const name = "Caner Kus";
+  const email = "caner@gmail.com";
+  const password = "123456";
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
   res.status(200).json({
     success: true,
+    data: user,
   });
 };
 
