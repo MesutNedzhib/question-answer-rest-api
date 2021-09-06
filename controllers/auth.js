2740b139b40445e4b8b8f2be7e7ef8b8ad857ad4
swaperX <mesut.nedzhib@gmail.com>
2021-09-06 17:34:53 +0300

    commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,34 +1,31 @@
 const User = require("../models/User");
 const CustomError = require("../helpers/errors/CustomError");
+const asyncErrorHandler = require("express-async-handler");
 
-const register = async (req, res, next) => {
+const register = asyncErrorHandler(async (req, res, next) => {
   const name = "Caner Kus";
   const email = "caner@gmail.com";
   const password = "12345";
 
-  try {
-    const user = await User.create({
-      name,
-      email,
-      password,
-    });
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
 
-    res.status(200).json({
-      success: true,
-      data: user,
-    });
-  } catch (err) {
-    return next(err);
-  }
-};
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
 
-const errorTest = (req, res, next) => {
+const errorTest = asyncErrorHandler((req, res, next) => {
   // Some code
 
   // Question Does Not Exists
-  return next(new TypeError());
+  return next(new TypeError("TypeError"));
   // Some code
-};
+});
 
 module.exports = {
   register,
