1c48b251a4a26c80c7587a75fe19e51ccf20f13d
swaperX <mesut.nedzhib@gmail.com>
2021-09-07 18:20:10 +0300

    commit

--- helpers/authorization/tokenHelpers.js
+++ helpers/authorization/tokenHelpers.js
@@ -9,7 +9,7 @@ const sendJwtToClient = (user, res) => {
     .status(200)
     .cookie("access_token", token, {
       httpOnly: true,
-      expires: new Date(Date.now() + parseInt(JWT_COOKIE_EXPIRE) * 1000),
+      expires: new Date(Date.now() + parseInt(JWT_COOKIE_EXPIRE) * 1000 * 60),
       secure: NODE_ENV === "development" ? false : true,
     })
     .json({
@@ -28,4 +28,10 @@ const isTokenIncluded = (req) => {
   );
 };
 
-module.exports = { sendJwtToClient, isTokenIncluded };
+const getAccessTokenFromHeader = (req) => {
+  const authorization = req.headers.authorization;
+  const access_token = authorization.split(" ")[1];
+  return access_token;
+};
+
+module.exports = { sendJwtToClient, isTokenIncluded, getAccessTokenFromHeader };
--- middlewares/authorization/auth.js
+++ middlewares/authorization/auth.js
@@ -1,16 +1,33 @@
 const CustomError = require("../../helpers/errors/CustomError");
 const jwt = require("jsonwebtoken");
-const { isTokenIncluded } = require("../../helpers/authorization/tokenHelpers");
+const {
+  isTokenIncluded,
+  getAccessTokenFromHeader,
+} = require("../../helpers/authorization/tokenHelpers");
 const getAccessToRoute = (req, res, next) => {
+  const { JWT_SECRET_KEY } = process.env;
   // 401 , 403
   // 401 - Unauthorized
   // 403 - Forbidden
   // Token
   if (!isTokenIncluded(req)) {
-    return next(new CustomError("You are not authorized to access this route"));
+    return next(
+      new CustomError("You are not authorized to access this route", 401)
+    );
   }
+  const accessToken = getAccessTokenFromHeader(req);
+  jwt.verify(accessToken, JWT_SECRET_KEY, (err, decoded) => {
+    if (err)
+      return next(
+        new CustomError("You are not authorized to access this route", 401)
+      );
+    req.user = {
+      id: decoded.id,
+      name: decoded.name,
+    };
 
-  next();
+    next();
+  });
 };
 module.exports = {
   getAccessToRoute,
