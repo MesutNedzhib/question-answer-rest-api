6016e7b856ef39207f435d7b16677830b46ab51b
swaperX <mesut.nedzhib@gmail.com>
2021-09-08 14:43:39 +0300

    commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const routers = require("./routers/index");
 const connectDatabase = require("./helpers/database/connectDatabase");
 const customErrorHandler = require("./middlewares/errors/customErrorHandler");
+const path = require("path");
 
 const app = express();
 
@@ -27,6 +28,9 @@ app.use("/api", routers);
 
 // Error Handler
 app.use(customErrorHandler);
+
+// Static Files
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
   console.log(`App started on PORT: ${PORT} -> http://localhost:${PORT}`);
