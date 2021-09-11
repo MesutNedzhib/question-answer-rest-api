78be8f88853e5b77ea88b0f9a17a0e344bafe241
swaperX <mesut.nedzhib@gmail.com>
2021-09-11 12:47:36 +0300

    commit

--- controllers/question.js
+++ controllers/question.js
@@ -17,9 +17,68 @@ const askNewQuestion = expressAsyncHandler(async (req, res, next) => {
 });
 
 const getAllQuestions = expressAsyncHandler(async (req, res, next) => {
-  const questions = await Question.find();
+  let query = Question.find();
+  const populate = true;
+  const populateObject = {
+    path: "user",
+    select: "name profile_image",
+  };
+
+  // Search
+  if (req.query.search) {
+    const searchObject = {};
+    // Example Title
+    // searchValue
+
+    const regex = new RegExp(req.query.search, "i");
+    searchObject["title"] = regex;
+
+    query = query.where(searchObject);
+  }
+
+  // Populate
+  if (populate) {
+    query = query.populate(populateObject);
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 5;
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  const pagination = {};
+  const total = await Question.countDocuments();
+
+  if (startIndex > 0) {
+    pagination.previous = {
+      page: page - 1,
+      limit: limit,
+    };
+  }
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit: limit,
+    };
+  }
+
+  query = query.skip(startIndex).limit(limit);
+
+  // 1 2 3 4 5 6 7 8 9 10 - Questions
+  // page = 2, limit 5 - startIndex 2, endIndex 10
+  // skip(2)
+  // limit(2)
+
+  const questions = await query;
+  // const questions = await Question.find().where({title:"Questions 3 - Title"});
+
   res.status(200).json({
     success: true,
+    count: questions.length,
+    pagination: pagination,
     data: questions,
   });
 });
